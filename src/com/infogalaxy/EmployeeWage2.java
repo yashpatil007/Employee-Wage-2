319264ce48a497a70b04b7413403e79e2874af55
yashpatil007 <yashpatil85326@gmail.com>
2022-03-09 17:25:54 +0530

    MODIFIED: Employee Daily Wage Calculation

--- src/com/infogalaxy/EmployeeWage2.java
+++ src/com/infogalaxy/EmployeeWage2.java
@@ -4,24 +4,36 @@ import java.util.Random;
 
 public class EmployeeWage2 {
     public static void main(String[] args) {
-        Random random = new Random();
+
+        final int WAGE_PER_HR = 20;
         final int PART_TIME_HR = 4;
         final int FULL_TIME_HR = 8;
-        int empWage = 0;
+        Random random = new Random();
+        //UC-3 Checking for Employee Calculation of Month
+        for (int i = 1; i <= 20; i++) {
+            int empWage = 0;
+            System.out.println("Day=" +i);
             int empAttendance = random.nextInt() % 2;
+            System.out.println(empAttendance);
+            //UC-1 Checking for Employee Present or Absent
             if (empAttendance == 0) {
                 System.out.println("Employee is Absent");
             } else {
-                System.out.println("Emplyee is Present");
+                System.out.println("Employee is Present");
                 int empType = random.nextInt() % 2;
-                if(empAttendance == 0){
-                    System.out.println("Employee is on Part time");
-                    empWage = PART_TIME_HR * FULL_TIME_HR;
-                } else {
-                    System.out.println("Employee is on Full Time");
-                    empWage = PART_TIME_HR * FULL_TIME_HR;
+                //UC-2 Checking for Employee is on Part time or Full time
+                switch(empType) {
+                    case 0:
+                        System.out.println("Employee is on Part time");
+                        empWage = WAGE_PER_HR * FULL_TIME_HR;
+                        break;
+                    case 1:
+                        System.out.println("Employee is on Full Time");
+                        empWage = WAGE_PER_HR * FULL_TIME_HR;
+                        break;
                 }
-                    System.out.println("Total Wage="+empWage);
                 }
+                System.out.println("Total Wage=" + empWage);
             }
+        }
     }
